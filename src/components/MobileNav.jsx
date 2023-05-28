4a2303cef1d236b5e6b340425be3190dfc242c8e
sirandresitog <sirandresitog@gmail.com>
2023-05-28 15:44:20 -0500

    fix MobileNavbar

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -28,7 +28,7 @@ const MobileNav = () => {
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 20 20'
 					fill='currentColor'
-					className='text-black dark:text-gray-100'>
+					className='text-white'>
 					<path
 						fillRule='evenodd'
 						d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
@@ -37,7 +37,7 @@ const MobileNav = () => {
 				</svg>
 			</button>
 			<div
-				className={`fixed top-0 left-0 z-10 h-full w-full transform bg-slate-900 opacity-95 duration-300 ease-in-out dark:bg-dorado ${
+				className={`fixed top-0 left-0 z-10 h-full w-full transform opacity-95 duration-300 ease-in-out dark:bg-[#333] ${
 					navShow ? 'translate-x-0' : 'translate-x-full'
 				}`}>
 				<div className='flex justify-end'>
@@ -50,7 +50,7 @@ const MobileNav = () => {
 							xmlns='http://www.w3.org/2000/svg'
 							viewBox='0 0 20 20'
 							fill='currentColor'
-							className='text-gray-900 dark:text-gray-100'>
+							className='text-white'>
 							<path
 								fillRule='evenodd'
 								d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
@@ -66,7 +66,7 @@ const MobileNav = () => {
 							className='px-12 py-4'>
 							<Link
 								href={link.href}
-								className='text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100'
+								className='text-2xl font-bold tracking-widest text-white'
 								onClick={onToggleNav}>
 								{link.title}
 							</Link>
